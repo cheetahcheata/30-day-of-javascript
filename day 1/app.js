44391283fedcc0620c0de59a87c03d56d00c87ee
cheetahcheata <89013969+cheetahcheata@users.noreply.github.com>
2021-08-18 21:47:20 +0700

    Add day 1 practice
    - Learn DataType
    - console.log

---
+++ day 1/app.js
@@ -0,0 +1,9 @@
+    console.log('This is from app.js')
+let name = "Cherry"
+const age = 22
+    console.log(name, age)
+let firstname = "Socheata"
+let lastname = "Sok"
+let country = "Cambodia"
+let city = "Phnom Penh"
+    console.log (firstname, lastname, country, city)
