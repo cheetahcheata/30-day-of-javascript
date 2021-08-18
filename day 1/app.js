08a8c9caf17d3fc30f0f481fb4c5310514f12901
cheetahcheata <89013969+cheetahcheata@users.noreply.github.com>
2021-08-18 21:50:27 +0700

    Clean code

--- day 1/app.js
+++ day 1/app.js
@@ -1,9 +1,12 @@
-    console.log('This is from app.js')
+console.log('This is from app.js')
+
 let name = "Cherry"
 const age = 22
-    console.log(name, age)
+console.log(name, age)
+
 let firstname = "Socheata"
 let lastname = "Sok"
 let country = "Cambodia"
 let city = "Phnom Penh"
-    console.log (firstname, lastname, country, city)
+
+console.log (firstname, lastname, country, city)
